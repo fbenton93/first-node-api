b554322b55b229e71183d5db403d58b1643be542
github username <github email address>
2019-01-16 16:46:15 -0500

    added CRUD paths for notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,54 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = function (app,db) {
+  app.get('/notes/:id', (req,res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) }
+    db.collection('notes').findOne(details, (err,result) => {
+      if(err) {
+        res.send({ error: 'There was an error'});
+      } else {
+        res.send(result);
+      }
+    })
+  })
+
+  app.delete('/notes/:id', (req,res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details,(err,result) => {
+      if(err) {
+        res.send({ error: 'There was an error'});
+      } else {
+        res.send(`Note with ID: ${id} deleted.`)
+      }
+    })
+  })
+
+  app.put('/notes/:id',(req,res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id)};
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details,note, (err,result) => {
+      if(err) {
+        res.send({ error: 'There was an error'})
+      } else {
+        res.send(note);
+      }
+    })
+  })
+
+
+
+
   app.post('/notes', (req,res) => {
-    // create the note here
-    // req will contain the parameters for the new note
-    console.log(req.body)
-    res.send('Completed');
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insertOne(note, (err,result) => {
+      if(err) {
+        res.send({error: 'An error has occurred.'});
+      } else {
+        res.send(result.ops[0]);
+      }
+    })
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+MongoClient.connect(db.url, (err,database) => {
+  if(err) return console.log(err);
+  const db = database.db('first-node-api');
+  require('./app/routes')(app,db);
 
 
-
-require('./app/routes')(app,{});
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  })
 })
